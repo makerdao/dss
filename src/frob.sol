0eb801874d3ca39b4fdb3073934662fa625af433
rain <rainbreak@riseup.net>
2018-08-04 16:08:13 +0100

    force distinct storage slots

--- src/frob.sol
+++ src/frob.sol
@@ -5,9 +5,9 @@ pragma solidity ^0.4.24;
 import './tune.sol';
 
 contract Lad {
-    bool  public live;
     Vat   public  vat;
     int   public Line;
+    bool  public live;
 
     constructor(address vat_) public { vat = Vat(vat_); live = true; }
 
