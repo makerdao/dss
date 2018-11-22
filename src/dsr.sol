ffa4622aa93f6f282a679fe2e9426ce115ca00bb
rain <rainbreak@riseup.net>
2018-11-22 06:30:36 +0000

    init

---
+++ src/dsr.sol
@@ -0,0 +1,139 @@
+pragma solidity ^0.4.24;
+
+import "ds-note/note.sol";
+
+/*
+   "Savings Dai" is obtained when Dai is deposited into
+   this contract. Each "Savings Dai" accrues Dai interest
+   at the "Dai Savings Rate".
+
+   This contract does not implement a user tradeable token
+   and is intended to be used with adapters.
+
+         --- `save` your `dai` in the `pot` ---
+
+   - `dsr`: the Dai Savings Rate
+   - `pie`: user balance of Savings Dai
+
+   - `save`: deposit / withdraw savings dai
+   - `drip`: perform rate collection
+   - `move`: transfer savings dai (for use by adapters)
+
+*/
+
+contract VatLike {
+    function move(bytes32,bytes32,int256) public;
+    function heal(bytes32,bytes32,int256) public;
+}
+
+contract Pot is DSNote {
+    // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address guy) public note auth { wards[guy] = 1; }
+    function deny(address guy) public note auth { wards[guy] = 0; }
+    modifier auth { require(wards[msg.sender] == 1); _; }
+
+    // --- Data ---
+    mapping (bytes32 => uint256) public pie;  // user Savings Dai
+
+    uint256 public Pie;  // total Savings Dai
+    uint256 public dsr;  // The Dai Savings Rate
+    uint256 public chi;  // The Rate Accumulator
+
+    VatLike public vat;  // CDP engine
+    bytes32 public vow;  // Debt engine
+    uint48  public rho;  // Time of last drip
+
+    // --- Init ---
+    constructor(address vat_) public {
+        wards[msg.sender] = 1;
+        vat = VatLike(vat_);
+        dsr = ONE;
+        chi = ONE;
+    }
+
+    // --- Math ---
+    uint256 constant ONE = 10 ** 27;
+    function rpow(uint x, uint n, uint base) internal pure returns (uint z) {
+      assembly {
+        switch x case 0 {switch n case 0 {z := base} default {z := 0}}
+        default {
+          switch mod(n, 2) case 0 { z := base } default { z := x }
+          let half := div(base, 2)  // for rounding.
+          for { n := div(n, 2) } n { n := div(n,2) } {
+            let xx := mul(x, x)
+            if iszero(eq(div(xx, x), x)) { revert(0,0) }
+            let xxRound := add(xx, half)
+            if lt(xxRound, xx) { revert(0,0) }
+            x := div(xxRound, base)
+            if mod(n,2) {
+              let zx := mul(z, x)
+              if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) { revert(0,0) }
+              let zxRound := add(zx, half)
+              if lt(zxRound, zx) { revert(0,0) }
+              z := div(zxRound, base)
+            }
+          }
+        }
+      }
+    }
+    function add(uint x, int y) internal pure returns (uint z) {
+      assembly {
+        z := add(x, y)
+        if sgt(y, 0) { if iszero(gt(z, x)) { revert(0, 0) } }
+        if slt(y, 0) { if iszero(lt(z, x)) { revert(0, 0) } }
+      }
+    }
+    function sub(uint x, int y) internal pure returns (uint z) {
+      assembly {
+        z := sub(x, y)
+        if slt(y, 0) { if iszero(gt(z, x)) { revert(0, 0) } }
+        if sgt(y, 0) { if iszero(lt(z, x)) { revert(0, 0) } }
+      }
+    }
+    function mul(uint x, int y) internal pure returns (int z) {
+      assembly {
+        z := mul(x, y)
+        if slt(x, 0) { revert(0, 0) }
+        if iszero(eq(y, 0)) { if iszero(eq(sdiv(z, y), x)) { revert(0, 0) } }
+      }
+    }
+    function sub(uint x, uint y) internal pure returns (int z) {
+        z = int(x) - int(y);
+        require(int(x) >= 0 && int(y) >= 0);
+    }
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = x * y;
+        require(y == 0 || z / y == x);
+        z = z / ONE;
+    }
+
+    // --- Administration ---
+    function file(bytes32 what, uint256 data) public note auth {
+        if (what == "dsr") dsr = data;
+    }
+    function file(bytes32 what, bytes32 data) public note auth {
+        if (what == "vow") vow = data;
+    }
+
+    // --- Savings Rate Accumulation ---
+    function drip() public note {
+        require(now >= rho);
+        int chi_ = sub(rmul(rpow(dsr, now - rho, ONE), chi), chi);
+        chi = add(chi, chi_);
+        rho  = uint48(now);
+        vat.heal(vow, bytes32(address(this)), -mul(Pie, chi_));
+    }
+
+    // --- Savings Dai Management ---
+    function save(int wad) public note {
+        bytes32 guy = bytes32(msg.sender);
+        pie[guy] = add(pie[guy], wad);
+        Pie      = add(Pie,      wad);
+        vat.move(guy, bytes32(address(this)), mul(chi, wad));
+    }
+    function move(bytes32 src, bytes32 dst, int wad) public auth {
+        pie[src] = sub(pie[src], wad);
+        pie[dst] = add(pie[dst], wad);
+    }
+}
---
+++ src/dsr.t.sol
@@ -0,0 +1,90 @@
+pragma solidity ^0.4.24;
+
+import "ds-test/test.sol";
+import "dss/tune.sol";
+import './dsr.sol';
+
+contract Hevm {
+    function warp(uint256) public;
+}
+
+contract DSRTest is DSTest {
+    Hevm hevm;
+
+    Vat vat;
+    Pot pot;
+
+    bytes32 self;
+    bytes32 potb;
+
+    function rad(uint wad_) internal pure returns (uint) {
+        return wad_ * 10 ** 27;
+    }
+    function wad(uint rad_) internal pure returns (uint) {
+        return rad_ / 10 ** 27;
+    }
+
+    function setUp() public {
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm.warp(0);
+
+        vat = new Vat();
+        pot = new Pot(vat);
+        vat.rely(pot);
+        self = bytes32(address(this));
+        potb = bytes32(address(pot));
+
+        pot.file("vow", "vow");
+
+        vat.heal(self, self, -int(rad(100 ether)));
+    }
+    function test_save_0d() public {
+        assertEq(vat.dai(self), rad(100 ether));
+
+        pot.save(int(100 ether));
+        assertEq(wad(vat.dai(self)),   0 ether);
+        assertEq(pot.pie(self),      100 ether);
+
+        pot.save(int(-100 ether));
+        assertEq(wad(vat.dai(self)), 100 ether);
+    }
+    function test_save_1d() public {
+        pot.save(int(100 ether));
+        pot.file("dsr", uint(1000000564701133626865910626));  // 5% / day
+        hevm.warp(1 days);
+        pot.drip();
+        assertEq(pot.pie(self), 100 ether);
+        pot.save(int(-100 ether));
+        assertEq(wad(vat.dai(self)), 105 ether);
+    }
+    function test_drip_multi() public {
+        pot.save(int(100 ether));
+        pot.file("dsr", uint(1000000564701133626865910626));  // 5% / day
+        hevm.warp(1 days);
+        pot.drip();
+        assertEq(wad(vat.dai(potb)),   105 ether);
+        pot.file("dsr", uint(1000001103127689513476993127));  // 10% / day
+        hevm.warp(2 days);
+        pot.drip();
+        assertEq(wad(vat.sin("vow")), 15.5 ether);
+        assertEq(wad(vat.dai(potb)), 115.5 ether);
+        assertEq(pot.Pie(),          100   ether);
+        assertEq(pot.chi() / 10 ** 9, 1.155 ether);
+    }
+    function test_save_multi() public {
+        pot.save(int(100 ether));
+        pot.file("dsr", uint(1000000564701133626865910626));  // 5% / day
+        hevm.warp(1 days);
+        pot.drip();
+        pot.save(-int(50 ether));
+        assertEq(wad(vat.dai(self)), 52.5 ether);
+        assertEq(pot.Pie(),          50.0 ether);
+
+        pot.file("dsr", uint(1000001103127689513476993127));  // 10% / day
+        hevm.warp(2 days);
+        pot.drip();
+        pot.save(-int(50 ether));
+        assertEq(wad(vat.dai(self)), 110.25 ether);
+        assertEq(pot.Pie(),            0.00 ether);
+    }
+}
