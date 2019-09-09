023223502a3d644eb046aad30360ff2766ca3fbc
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-09-09 15:02:36 -0600

    update meme to pad

--- src/test/flop.t.sol
+++ src/test/flop.t.sol
@@ -147,7 +147,7 @@ contract FlopTest is DSTest {
         assertTrue( Guy(ali).try_tick(id));
         // check biddable
         (, uint _lot,,,) = flop.bids(id);
-        // tick should increase the lot by beg (50%) and restart the auction
+        // tick should increase the lot by pad (50%) and restart the auction
         assertEq(_lot, 300 ether);
         assertTrue( Guy(ali).try_dent(id, 100 ether, 10 ether));
     }
