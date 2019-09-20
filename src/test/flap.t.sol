35f85c65b8e2586a404778adbd6f3c7d015c42b9
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-09-20 15:05:09 -0400

    fix incorrect line deletion

--- src/test/flap.t.sol
+++ src/test/flap.t.sol
@@ -56,7 +56,7 @@ contract FlapTest is DSTest {
         gem = new DSToken('');
 
         fuss = new Flapper(address(vat), address(gem));
-        fuss.rely(address(this));
+        flap = address(fuss);
 
         ali = address(new Guy(fuss));
         bob = address(new Guy(fuss));
