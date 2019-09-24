801d59c405b313315fbaa4ddebc6edf216e8b27a
Christopher Mooney <chris@dod.net>
2019-09-24 15:00:04 -0400

    ISSUE-335: Explicitly declare compiler version

--- src/vat.sol
+++ src/vat.sol
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity >=0.5.0;
+pragma solidity =0.5.11;
 
 contract Vat {
     // --- Auth ---
