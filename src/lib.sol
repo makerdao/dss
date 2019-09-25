a2e6f064681039c65b0afc563a39c668bab689ff
Christopher Mooney <chris@dod.net>
2019-09-25 09:39:57 -0400

    ISSUE-335: Explicitly declare compiler version (#71)
    * ISSUE-335: Explicitly declare compiler version
    
    * need to lock the version travis compiles with

--- src/lib.sol
+++ src/lib.sol
@@ -11,7 +11,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity >=0.4.23;
+pragma solidity =0.5.11;
 
 contract DSNote {
     event LogNote(
