b0f12eb49b96d2227718b3269dc3b6ee1734fe24
Anton Bukov <k06aaa@gmail.com>
2019-10-19 21:54:27 +0300

    Replace keccak256 in Solidity constant with compile-time constant for performance, since Solidity 0.5.x constants works as pure functions

--- src/dai.sol
+++ src/dai.sol
@@ -48,7 +48,7 @@ contract Dai is DSNote {
 
     // --- EIP712 niceties ---
     bytes32 public DOMAIN_SEPARATOR;
-    bytes32 public PERMIT_TYPEHASH;
+    bytes32 public constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
 
     constructor(uint256 chainId_) public {
         wards[msg.sender] = 1;
@@ -59,9 +59,6 @@ contract Dai is DSNote {
             chainId_,
             address(this)
         ));
-        PERMIT_TYPEHASH = keccak256(
-            "Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)"
-        );
     }
 
     // --- Token ---
