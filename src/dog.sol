4a155f446ec636968248f1318ea7e0ac434f4cf4
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-01-06 13:09:36 -0300

    Compile with solc 0.6.11

--- src/dog.sol
+++ src/dog.sol
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity >=0.6.7;
+pragma solidity >=0.6.11;
 
 interface ClipperLike {
     function kick(uint256 tab, uint256 lot, address usr) external returns (uint256);
