ad736dbd2215353344e28a13f27d6c2037e759bd
Emilio Silva Schlenker <e18r@disroot.org>
2021-01-20 17:50:39 -0500

    add isDusty() function and a few tests
    - test_bark_basic()
    - testFail_bark_not_unsafe()
    - test_bark_unliquidatable_vault()
    - test_bark_dust_over_ilk_hole()
    - test_bark_dust_over_Hole()
    - test_bark_exactly_dust_over_Hole()

--- src/test/dog.t.sol
+++ src/test/dog.t.sol
@@ -20,7 +20,7 @@ contract ClipperMock {
 contract DogTest is DSTest {
 
     bytes32 constant ilk = "gold";
-    address constant usr = address(1);
+    address constant usr = address(1337);
     uint256 constant THOUSAND = 1E3;
     uint256 constant WAD = 1E18;
     uint256 constant RAY = 1E27;
@@ -42,22 +42,77 @@ contract DogTest is DSTest {
         dog.file(ilk, "chop", 11 * WAD / 10);
         dog.file("vow", address(vow));
         dog.file(ilk, "clip", address(clip));
+        dog.file("Hole", 10 * THOUSAND * RAD);
+        dog.file(ilk, "hole", 10 * THOUSAND * RAD);
     }
 
     function setUrn(uint256 ink, uint256 art) internal {
         vat.slip(ilk, usr, int256(ink));
         vat.suck(address(vow), address(vow), art * RAY);
         vat.grab(ilk, usr, usr, address(vow), int256(ink), int256(art));
+        (uint256 actualInk, uint256 actualArt) = vat.urns(ilk, usr);
+        assertEq(ink, actualInk);
+        assertEq(art, actualArt);
     }
 
-    function setHoles(uint256 Hole, uint256 hole) internal {
-        dog.file("Hole", Hole);
-        dog.file(ilk, "hole", hole);
+    function isDusty() internal returns (bool dusty) {
+        (, uint256 rate,,, uint256 dust) = vat.ilks(ilk);
+        (, uint256 art) = vat.urns(ilk, usr);
+        (, uint256 chop,,,,) = dog.ilks(ilk);
+        uint256 due = art * rate;
+        uint256 tab = due * chop / WAD;
+        dusty = tab > 0 && tab < dust;
     }
 
-    function test_dog() public {
+    function test_bark_basic() public {
         setUrn(WAD, 2 * THOUSAND * WAD);
-        setHoles(10 * THOUSAND * RAD, 10 * THOUSAND * RAD);
-        dog.bark(ilk, usr, usr);
+        dog.bark(ilk, usr, address(this));
+        (uint256 ink, uint256 art) = vat.urns(ilk, usr);
+        assertEq(ink, 0);
+        assertEq(art, 0);
+    }
+
+    function testFail_bark_not_unsafe() public {
+        setUrn(WAD, 500 * WAD);
+        dog.bark(ilk, usr, address(this));
+    }
+
+    // currently, dog.bark doesn't check if a vault is unliquidatable
+    function test_bark_unliquidatable_vault() public {
+        uint256 dust = 200;
+        vat.file(ilk, "dust", dust * RAD);
+        setUrn(1, (dust / 2) * WAD);
+        assertTrue(isDusty());
+        dog.bark(ilk, usr, address(this));
+    }
+
+    function test_bark_dust_over_ilk_hole() public {
+        uint256 dust = 200;
+        vat.file(ilk, "dust", dust * RAD);
+        uint256 hole = 5 * THOUSAND;
+        dog.file(ilk, "hole", hole * RAD);
+        setUrn(WAD, (hole + dust / 2) * WAD);
+        dog.bark(ilk, usr, address(this));
+        assertTrue(!isDusty());
+    }
+
+    function test_bark_dust_over_Hole() public {
+        uint256 dust = 200;
+        vat.file(ilk, "dust", dust * RAD);
+        uint256 Hole = 5 * THOUSAND;
+        dog.file("Hole", Hole * RAD);
+        setUrn(WAD, (Hole + dust / 2) * WAD);
+        dog.bark(ilk, usr, address(this));
+        assertTrue(!isDusty());
+    }
+
+    function test_bark_exactly_dust_over_Hole() public {
+        uint256 dust = 200;
+        vat.file(ilk, "dust", dust * RAD);
+        uint256 Hole = 5 * THOUSAND;
+        dog.file("Hole", Hole * RAD);
+        setUrn(WAD, (Hole + dust) * WAD);
+        dog.bark(ilk, usr, address(this));
+        assertTrue(!isDusty());
     }
 }
