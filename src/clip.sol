69d8d309aa9159a7c3388eda24892701596f8e42
Brian McMichael <brian@brianmcmichael.com>
2021-01-20 19:27:29 -0500

    remove payer comment

--- src/clip.sol
+++ src/clip.sol
@@ -251,7 +251,7 @@ contract Clipper {
     function take(uint256 id,           // Auction id
                   uint256 amt,          // Upper limit on amount of collateral to buy  [wad]
                   uint256 max,          // Maximum acceptable price (DAI / collateral) [ray]
-                  address who,          // Receiver of collateral, payer of DAI, and external call address
+                  address who,          // Receiver of collateral, and external call address
                   bytes calldata data   // Data to pass in external call; if length 0, no call is done
     ) external lock isStopped(2) {
 
