9739ff94bea1c8fac419c91187a60c07596c18fd
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-03-24 11:52:36 -0400

    fix: spot copyright

--- src/spot.sol
+++ src/spot.sol
@@ -2,8 +2,6 @@
 
 /// spot.sol -- Spotter
 
-// Copyright (C) 2022 Dai Foundation
-
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
