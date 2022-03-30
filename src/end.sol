1f059364f07443149b8809b39105f7346ef7a6a1
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-03-30 10:00:51 -0400

    docs: bring back MEGH copyright in the End

--- src/end.sol
+++ src/end.sol
@@ -4,6 +4,7 @@
 
 // Copyright (C) 2018 Rain <rainbreak@riseup.net>
 // Copyright (C) 2018 Lev Livnev <lev@liv.nev.org.uk>
+// Copyright (C) 2020-2021 Maker Ecosystem Growth Holdings, INC.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
