309380bb74b4bc10962d47a749a37bd7b4897c97
Yassine <yassine.kachrad@gmail.com>
2022-11-14 22:20:50 +0100

    version fix

--- src/vat.sol
+++ src/vat.sol
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity ^0.6.12;
+pragma solidity >=0.6.12;
 
 // FIXME: This contract was altered compared to the production version.
 // It doesn't use LibNote anymore.
